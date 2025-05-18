84a7a7c7d74000c616afcb5a6daebd088731e981
JericDev <162023159+JericDev@users.noreply.github.com>
2025-05-19 00:40:16 +0800

    Update batmanslap.js

--- Priyansh/commands/batmanslap.js
+++ Priyansh/commands/batmanslap.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "batslap",
     version: "2.0.0",
-    hasPermssion: 0,
+    hasPermssion: 2,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "",
     commandCategory: "general",
